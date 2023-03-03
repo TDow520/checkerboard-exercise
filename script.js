31971cb05d383fe837782960708775770c543f1c
TDow520 <dow.tailon@gmail.com>
2023-03-03 09:13:38 -0600

    First commit.  Got to dispaly red and black tiles. Still working on stoping the dispaly to a 9 x 9 square board.

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 // Your JS goes here
+
+let count = 0
+
+
+
+function sq(){
+    let square = document.createElement('div');
+    square.style.width = '11.1%';
+    square.style.paddingBottom = '11.1%'
+    square.style.float = 'left';
+    return square;
+    
+
+}
+
+
+function buildCheckerBoard(count){
+    let redSq = sq();
+    redSq.style.backgroundColor = 'red';
+    let blkSq = sq();
+    blkSq.style.backgroundColor = 'black';
+    document.body.append(redSq)
+    
+    console.log(count)  
+    document.body.append(blkSq); 
+    count = count + 1
+    console.log(count)
+    }
+    
+
+
+
+while (count < 82){
+    buildCheckerBoard(count)
+
+}
+
+
